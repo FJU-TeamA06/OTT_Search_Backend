8956543106188fef864673a3733cb4f82e9d7979
andyching168 <andyching168@gmail.com>
2022-10-30 22:30:39 +0800

    小修正

--- search.py
+++ search.py
@@ -121,21 +121,21 @@ def searchOTT(text):
 		for i in range(0,len(res['result'])):
 			df = pd.DataFrame()
 			if res['result'][i]['channel']['id'] == "UCgdwtyqBunlRb-i-7PnCssQ":
-				print("MuseYT")
-				print(res['result'][i]['title'])
-				print(res['result'][i]['link'])
+				#print("MuseYT")
+				#print(res['result'][i]['title'])
+				#print(res['result'][i]['link'])
 				url=res['result'][i]['link']
 				df = df.append({"Platform":"MuseYT","URL":url}, ignore_index=True)
-				print(df)
+				#print(df)
 				dfAll=dfAll.append({"Title":res['result'][i]['title'],"Watch":df}, ignore_index=True)
 
 			if res['result'][i]['channel']['id'] == "UC0wNSTMWIL3qaorLx0jie6A":
-				print("AniOneYT")
-				print(res['result'][i]['title'])
-				print(res['result'][i]['link'])
+				#print("AniOneYT")
+				#print(res['result'][i]['title'])
+				#print(res['result'][i]['link'])
 				url=res['result'][i]['link']
 				df = df.append({"Platform":"AniOneYT","URL":url}, ignore_index=True)
-				print(df)
+				#print(df)
 				dfAll=dfAll.append({"Title":res['result'][i]['title'],"Watch":df}, ignore_index=True)
 	except:                   # 如果 try 的內容發生錯誤，就執行 except 裡的內容
 		pass	
