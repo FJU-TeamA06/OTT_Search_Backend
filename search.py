440c32fcef07900a33f067f2b37a726588ff9b41
andyching168 <andyching168@gmail.com>
2022-12-07 10:29:38 +0800

    防止SQL注入和防止亂輸入而錯誤

--- search.py
+++ search.py
@@ -12,135 +12,137 @@ def searchOTT(text):
 	dfAll = pd.DataFrame()
 
 	SearchKeyword=text
+	try:
+		#justwatch(用justwatchAPI抓取)
+		just_watch = JustWatch(country='TW')
+		results = just_watch.search_for_item(query=SearchKeyword)
+		str1 = json.dumps(results)
+		str2 = json.dumps(results['items'])
+		if results['total_results']>=3:
+			max=3
+		else:
+			max=results['total_results']
+		for i in range(0,max):
 
-	#justwatch(用justwatchAPI抓取)
-	just_watch = JustWatch(country='TW')
-	results = just_watch.search_for_item(query=SearchKeyword)
-	str1 = json.dumps(results)
-	str2 = json.dumps(results['items'])
-	if results['total_results']>=3:
-		max=3
-	else:
-		max=results['total_results']
-	for i in range(0,max):
+			title=results['items'][i]['title']
+			#print(title)
+			#df = pd.DataFrame(results['items'][i]['offers'])
+			#print(df.columns)
+			#print("=====================")
+			try:
+				for j in range(len(results['items'][i]['offers'])):
+					url=results['items'][i]['offers'][j]['urls']['standard_web']
+					platform=results['items'][i]['offers'][j]['package_short_name']
+					#print(platform)
+					#print(url)
+					dfAll=dfAll.append({"Platform":platform,"Title":title,"Tag":SearchKeyword,"URL":url}, ignore_index=True)
+					dfAll=dfAll.append({"Platform":platform,"Title":title,"Tag":title,"URL":url}, ignore_index=True)
+					dfAll.drop_duplicates(inplane=True)
+			except:                   # 如果 try 的內容發生錯誤，就執行 except 裡的內容
+				pass
+			#print("***********")
 
-		title=results['items'][i]['title']
-		#print(title)
-		#df = pd.DataFrame(results['items'][i]['offers'])
-		#print(df.columns)
-		#print("=====================")
+
+		#BiliBili(用bilibili_api抓取)
+		async def test_f_search_by_order():
+		    return await search.search_by_type(SearchKeyword, search_type=search.SearchObjectType.BANGUMI)
+		listTitle=[]
+		listUrl=[]
+
+		res = sync(test_f_search_by_order())
+		#print(res['result'])
 		try:
-			for j in range(len(results['items'][i]['offers'])):
-				url=results['items'][i]['offers'][j]['urls']['standard_web']
-				platform=results['items'][i]['offers'][j]['package_short_name']
-				#print(platform)
+			str1 = json.dumps(res['result'])
+			#print(str1)
+			if len(res['result'])>=5:
+				max=5
+			else:
+				max=len(res['result'])
+
+			for i in range(0,max):
+				title=res['result'][i]['title']
+
+				title = title.replace('<em class="keyword">',"")
+				title = title.replace('</em>',"")
+				#print(title)
+				url=res['result'][i]['url']
+				dfAll=dfAll.append({"Platform":"BiliBili","Title":title,"Tag":title,"URL":url}, ignore_index=True)
+				dfAll=dfAll.append({"Platform":"BiliBili","Title":title,"Tag":SearchKeyword,"URL":url}, ignore_index=True)
+
+				#dfAll.drop_duplicates(subset='URL',inplace=True)
+				#print(title)
 				#print(url)
-				dfAll=dfAll.append({"Platform":platform,"Title":title,"Tag":SearchKeyword,"URL":url}, ignore_index=True)
-				dfAll=dfAll.append({"Platform":platform,"Title":title,"Tag":title,"URL":url}, ignore_index=True)
-				dfAll.drop_duplicates(inplane=True)
+				#print("-----")
 		except:                   # 如果 try 的內容發生錯誤，就執行 except 裡的內容
 			pass
-		#print("***********")
 
 
-	#BiliBili(用bilibili_api抓取)
-	async def test_f_search_by_order():
-	    return await search.search_by_type(SearchKeyword, search_type=search.SearchObjectType.BANGUMI)
-	listTitle=[]
-	listUrl=[]
-
-	res = sync(test_f_search_by_order())
-	#print(res['result'])
-	try:
-		str1 = json.dumps(res['result'])
-		#print(str1)
-		if len(res['result'])>=5:
-			max=5
-		else:
-			max=len(res['result'])
-
-		for i in range(0,max):
-			title=res['result'][i]['title']
-
-			title = title.replace('<em class="keyword">',"")
-			title = title.replace('</em>',"")
-			#print(title)
-			url=res['result'][i]['url']
-			dfAll=dfAll.append({"Platform":"BiliBili","Title":title,"Tag":title,"URL":url}, ignore_index=True)
-			dfAll=dfAll.append({"Platform":"BiliBili","Title":title,"Tag":SearchKeyword,"URL":url}, ignore_index=True)
-
+		#動畫瘋(用BeautifulSoup爬取)
+		listTitle=[]
+		listUrl=[]
+		headers = {
+		    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+		}
+		response = requests.get(
+		    "https://ani.gamer.com.tw/search.php?keyword="+SearchKeyword, headers=headers)
+		soup = BeautifulSoup(response.text, "html.parser")
+		#print(soup.prettify())  #輸出排版後的HTML內容
+		result = soup.find("div", class_="theme-list-block")
+		titles=result.find_all("p",class_="theme-name")
+		urls=result.find_all("a", class_="theme-list-main")
+		#print(urls)
+		for title in titles:
+		    listTitle.append(title.text)
+		    #print(title.text)
+		for url in urls:
+		    listUrl.append(url.get("href"))
+		    #print(url.get("href"))
+		for i in range(0,len(listTitle)):
+			title=listTitle[i]
+			url="https://ani.gamer.com.tw/"+listUrl[i]
+			dfAll=dfAll.append({"Platform":"anime_gamer","Title":title,"Tag":title,"URL":url}, ignore_index=True)
+			dfAll=dfAll.append({"Platform":"anime_gamer","Title":title,"Tag":SearchKeyword,"URL":url}, ignore_index=True)
 			#dfAll.drop_duplicates(subset='URL',inplace=True)
-			#print(title)
-			#print(url)
-			#print("-----")
+
+		    #print(listTitle[i])
+		    #print("https://ani.gamer.com.tw/"+listUrl[i])
+		#Youtube搜尋(羚邦&木棉花)
+		searchYT = PlaylistsSearch(SearchKeyword)
+		res=searchYT.result()
+		try:
+			for i in range(0,len(res['result'])):
+				df = pd.DataFrame()
+				if res['result'][i]['channel']['id'] == "UCgdwtyqBunlRb-i-7PnCssQ":
+					#print("MuseYT")
+					#print(res['result'][i]['title'])
+					#print(res['result'][i]['link'])
+					title=res['result'][i]['title']
+					url=res['result'][i]['link']
+
+					dfAll=dfAll.append({"Platform":"MuseYT","Title":title,"Tag":title,"URL":url}, ignore_index=True)
+
+					dfAll=dfAll.append({"Platform":"MuseYT","Title":title,"Tag":SearchKeyword,"URL":url}, ignore_index=True)
+					#dfAll.drop_duplicates(subset='URL',inplace=True)
+
+				if res['result'][i]['channel']['id'] == "UC0wNSTMWIL3qaorLx0jie6A":
+					#print("AniOneYT")
+					#print(res['result'][i]['title'])
+					#print(res['result'][i]['link'])
+					title=res['result'][i]['title']
+					url=res['result'][i]['link']
+					dfAll=dfAll.append({"Platform":"AniOneYT","Title":title,"Tag":title,"URL":url}, ignore_index=True)
+					dfAll=dfAll.append({"Platform":"AniOneYT","Title":title,"Tag":SearchKeyword,"URL":url}, ignore_index=True)
+					#dfAll.drop_duplicates(subset='URL',inplace=True)
+		except:                   # 如果 try 的內容發生錯誤，就執行 except 裡的內容
+			pass	
+
+
+
+
+
+		#print(dfAll)
+		#print("/////////////////")
+		#print(dfAll.to_json())
 	except:                   # 如果 try 的內容發生錯誤，就執行 except 裡的內容
-		pass
-
-
-	#動畫瘋(用BeautifulSoup爬取)
-	listTitle=[]
-	listUrl=[]
-	headers = {
-	    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
-	}
-	response = requests.get(
-	    "https://ani.gamer.com.tw/search.php?keyword="+SearchKeyword, headers=headers)
-	soup = BeautifulSoup(response.text, "html.parser")
-	#print(soup.prettify())  #輸出排版後的HTML內容
-	result = soup.find("div", class_="theme-list-block")
-	titles=result.find_all("p",class_="theme-name")
-	urls=result.find_all("a", class_="theme-list-main")
-	#print(urls)
-	for title in titles:
-	    listTitle.append(title.text)
-	    #print(title.text)
-	for url in urls:
-	    listUrl.append(url.get("href"))
-	    #print(url.get("href"))
-	for i in range(0,len(listTitle)):
-		title=listTitle[i]
-		url="https://ani.gamer.com.tw/"+listUrl[i]
-		dfAll=dfAll.append({"Platform":"anime_gamer","Title":title,"Tag":title,"URL":url}, ignore_index=True)
-		dfAll=dfAll.append({"Platform":"anime_gamer","Title":title,"Tag":SearchKeyword,"URL":url}, ignore_index=True)
-		#dfAll.drop_duplicates(subset='URL',inplace=True)
-
-	    #print(listTitle[i])
-	    #print("https://ani.gamer.com.tw/"+listUrl[i])
-	#Youtube搜尋(羚邦&木棉花)
-	searchYT = PlaylistsSearch(SearchKeyword)
-	res=searchYT.result()
-	try:
-		for i in range(0,len(res['result'])):
-			df = pd.DataFrame()
-			if res['result'][i]['channel']['id'] == "UCgdwtyqBunlRb-i-7PnCssQ":
-				#print("MuseYT")
-				#print(res['result'][i]['title'])
-				#print(res['result'][i]['link'])
-				title=res['result'][i]['title']
-				url=res['result'][i]['link']
-
-				dfAll=dfAll.append({"Platform":"MuseYT","Title":title,"Tag":title,"URL":url}, ignore_index=True)
-
-				dfAll=dfAll.append({"Platform":"MuseYT","Title":title,"Tag":SearchKeyword,"URL":url}, ignore_index=True)
-				#dfAll.drop_duplicates(subset='URL',inplace=True)
-
-			if res['result'][i]['channel']['id'] == "UC0wNSTMWIL3qaorLx0jie6A":
-				#print("AniOneYT")
-				#print(res['result'][i]['title'])
-				#print(res['result'][i]['link'])
-				title=res['result'][i]['title']
-				url=res['result'][i]['link']
-				dfAll=dfAll.append({"Platform":"AniOneYT","Title":title,"Tag":title,"URL":url}, ignore_index=True)
-				dfAll=dfAll.append({"Platform":"AniOneYT","Title":title,"Tag":SearchKeyword,"URL":url}, ignore_index=True)
-				#dfAll.drop_duplicates(subset='URL',inplace=True)
-	except:                   # 如果 try 的內容發生錯誤，就執行 except 裡的內容
-		pass	
-
-
-
-
-
-	#print(dfAll)
-	#print("/////////////////")
-	#print(dfAll.to_json())
+			pass
 	return dfAll
--- searchDB.py
+++ searchDB.py
@@ -5,6 +5,9 @@ import warnings
 import csv
 import json
 import time
+from opencc import OpenCC
+
+cc = OpenCC('s2tw')
 warnings.filterwarnings("ignore")
 #資料庫連線設定
 #可縮寫db = pymysql.connect("localhost","root","root","30days" )
@@ -30,57 +33,37 @@ def searchDB(text):
         #cursor.execute(sql)
         #print(result_args)
     #args=(keyword)
-    cursor.execute("CALL SearchTag(\""+keyword+"\");")
     ResultPD=pd.DataFrame()
-    for result in cursor.fetchall():
-        #print(result[1]+"_"+result[3]+"_"+result[4])
-        ResultPD=ResultPD.append({"Platform":result[3],"Title":result[1],"URL":result[4]}, ignore_index=True)
+    ResultPD_Sorted=pd.DataFrame()
+    try:
+        sql="CALL SearchTag(%s);"
+        cursor.execute(sql,(keyword,))
+        
+        for result in cursor.fetchall():
+            #print(result[1]+"_"+result[3]+"_"+result[4])
+            platform=result[3]
+            print(platform)
+            platform=platform.replace("anime_gamer", "巴哈姆特動畫瘋")
+            platform=platform.replace("nfx","Netflix")
+            platform=platform.replace("gimytv","Gimy 劇迷")
+            platform=platform.replace("imaple","楓林網")
+            platform=platform.replace("line_TV","Line TV")
+            platform=platform.replace("lin","Line TV")
+            platform=platform.replace("kkt","KKTV")
+            platform=platform.replace("prv","Amazon Prime Video")
+            platform=platform.replace("dnp","Disney+")
+            platform=platform.replace("itu","iTunes")
+            platform=platform.replace("zee","Zee5")
+            platform=platform.replace("MuseYT","木棉花YouTube官方頻道")
+            platform=platform.replace("AniOneYT","羚邦YouTube官方頻道")
+            platform=platform.replace("mbi","MUBI")
+            platform=platform.replace("cla","Classix")
+            print(platform)
+            ResultPD=ResultPD.append({"Platform":platform,"Title":cc.convert(result[1]),"URL":result[4]}, ignore_index=True)
+        ResultPD_Sorted=ResultPD.sort_values(by='Platform')
+    except:                   # 如果 try 的內容發生錯誤，就執行 except 裡的內容
+            pass
     db.close()
-    ResultPD_Sorted=ResultPD.sort_values(by='Platform')
+    
     print(ResultPD_Sorted)
     return(ResultPD_Sorted.to_json(orient="table"));
-    '''
-    cursor.execute("call searchTag(\"給不滅\");")
-    for result in cursor.fetchall():
-        print(result)
-    
-    #return(WebSearchDF.to_json(orient="table"))
-    '''
-    '''
-    #SQL語法（查詢資料庫版本）
-    sql = "Select DISTINCT titletable.Mid,titletable.Title,tagstable.Tag,watchtable.Platform,watchtable.URL \
-    from tagstable, watchtable,titletable \
-    Where tagstable.Mid = watchtable.Mid and tagstable.Mid = titletable.Mid and tagstable.Tag like \"%"+keyword+"%\"";
-    try:
-        # Execute the SQL command
-        cursor.execute(sql)
-        # Fetch all the rows in a list of lists.
-        results = cursor.fetchall()
-        rowCount = len(results)
-        df = pd.DataFrame()
-        if rowCount != 0:
-            for row in results:
-                #print (row)
-                Mid = row[0]
-                Title=row[1]
-                Tag = row[2]
-                Platform = row[3]
-                URL = row[4]
-                # Now print fetched result
-                df=df.append({"Mid":Mid,"Title":Title,"Tag":Tag,"Platform":Platform,"URL":URL}, ignore_index=True)
-                print ("Mid = %s,Tag = %s,Platform = %s,URL = %s" % \
-                        (Mid, Tag,Platform, URL ))
-            fieldnames = ("Mid","Title","Tag","Platform","URL")
-            return(df.to_json(orient="table"))
-        else:
-            return("Not Found")
-                
-    except:
-       import traceback
-       traceback.print_exc()
-
-       return ("Error: unable to fetch data")
-
-    # disconnect from server
-    db.close()
-    '''
